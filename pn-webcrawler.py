24d28015b58bbe65ca8243aee597f788096c8e4b
philip94 <philipnassr2@yahoo.com>
2017-05-02 22:25:35 +0100

    Update pn-webcrawler.py

--- pn-webcrawler.py
+++ pn-webcrawler.py
@@ -15,9 +15,11 @@ import urllib2
 import subprocess
 
 siteList = []
+shortListedSites = []
 
 #Open webpage for domain name scraping
 def createSession(crawlingWebsite):
+    print "\nCrawler Initiated. Searching in '" + crawlingWebsite + "' for domains.\n\n"
     dryscrape.start_xvfb()
     #Begin new session with loaded scripts
     try:
@@ -27,14 +29,16 @@ def createSession(crawlingWebsite):
     	#Reset session for handling memory issues
     	session.reset()
     except InvalidResponseError as e:
-	print 'InvalidResponseError:', ed
+	print "Cannot open " + crawlingWebsite + "\n"
+	print 'InvalidResponseError:', e
+	quit()
     soup = BeautifulSoup(response, "html.parser")
     #Searches for hyperrefs in a page. This is the hardcoded bit.
     #Searching for items is webpage-specific. For a different website, please refer to its HTML content
     #to find out which tags are needed to obtain a list of domains if any.
     tableFound = soup.findAll("a", {"target": "_blank"})
 
-    if tableFound == None:
+    if len(tableFound) == 0:
         print "Nothing found. Terminating crawler."
 	quit()
     else:
@@ -49,8 +53,6 @@ def checkHTTPS():
     display = Display(visible = 0, size = (800, 600))
     display.start()
     httpsSiteList = []
-    httpSiteList = []
-    os.environ['geckodriver'] = "/home/osboxes/Downloads"
 
     for website in siteList:
         print website
@@ -64,73 +66,53 @@ def checkHTTPS():
 	    #This makes sure that whole page will be loaded before scraping.
             WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.CLASS_NAME, 'failed')))
             print website + " does NOT support HTTPS!"
+	    print "Moving on to the next website.\n\n"
             driver.quit() #Close browser
-            display.stop() #Close
-	    httpSiteList.append(website)
-        except TimeoutException:
+            display.stop() #Close display
+        except TimeoutException, NoSuchElementException:
             print website + " supports HTTPS!"
-	    driver.quit() #Close browser
-            display.stop() #Close
+	    driver.quit()
+            display.stop()
 	    httpsSiteList.append(website)
 	    checkAtlas(website)
-        except NoSuchElementException:
-            print "Such element does not exist!"
-	    driver.quit() #Close browser
-            display.stop() #Close
-	    httpsSiteList.append(website)
-	    checkAtlas(website)
-
-    print "Sites supporting HTTPS: " + str(len(httpsSiteList))
-    for ssite in httpsSiteList:
-        print ssite
-    print "\n"
-    print "Sites NOT supporting HTTPS: " + str(len(httpSiteList))
-    for nsite in httpSiteList:
-        print nsite
 
 #Check if site is present in the atlas and create ruleset
 def checkAtlas(wSite):
-    #print httpsList
-    shortListedSites = []
-    #for wSite in httpsList:
     hyperLinks = []
     try:
-        print wSite.lower()
 	siteChunks = wSite.lower().split(".")
-	for bits in siteChunks:
-	   print bits
-	if "http://www" in siteChunks[0] or "https://www" in siteChunks[0] or "www" in siteChunks[0]:
-	   del siteChunks[0]
-	   print siteChunks[0]
-	print siteChunks[0][0].lower()
+        if "www" in siteChunks[0]:
+           del siteChunks[0]
+	elif "http://" in siteChunks[0] or "https://" in siteChunks[0]:
+	   siteChunks[0] = siteChunks[0].split('/')[2]
         r = requests.get("http://www.eff.org/https-everywhere/atlas/letters/" + siteChunks[0][0].lower() + ".html")
-        print "Request accepted"
         resp = urllib2.urlopen(r.url)
-        print "URL Opened"
-        print r.url
         soup = BeautifulSoup(resp.read(), "html.parser")
-        print "Website scraped"
+
         domainList = soup.findAll('ul', id='domain-list')
+	#Loop through all domains starting with the first letter of the
+	#website currently being checked
         for link in domainList:
-	        #hyperLinks = []
             for site in link.findAll('li'):
                 hyperLinks.append(site.text)
-        print len(hyperLinks)
         for atlasSite in hyperLinks:
             if siteChunks[0].lower() == atlasSite.split(".")[0]:
-                print siteChunks[0].lower() + " is in the atlas.\n"
+                print siteChunks[0].lower() + " is in the atlas and already has a ruleset.\n\n"
                 break
             else:
 	        if atlasSite == hyperLinks[-1]:
-		   print siteChunks[0].lower() + " is NOT in the atlas. \n"
-		   ruleSite = ".".join(str(x) for x in siteChunks
-		   print ruleSite
-		   print ruleSite.split('/')[0]
-                   #shortListedSites.append(".".join(str(x) for x in siteChunks))
-    		   #subprocess.call(["bash make-trivial-rule " + ruleSite.split('/')[0]], shell = True, cwd = "/home/osboxes/Documents/https-everywhere/rules")
+		   print siteChunks[0].lower() + " is NOT in the atlas. Creating ruleset..."
+		   ruleSite = ".".join(str(x) for x in siteChunks)
+		   #Create ruleset for the website
+    		   ruleOutput = subprocess.call(["bash make-trivial-rule " + ruleSite.split('/')[0]], shell = True, cwd = "/home/osboxes/Documents/https-everywhere/rules")
+		   if (ruleOutput == 0):
+		      print "Successfully created a ruleset for '" + ruleSite + "'!\n\n"
+		      shortListedSites.append(ruleSite)
+		   else:
+		      print "Oops! '" + ruleSite + "' seems to already have a ruleset in your repository. Please examine it!\n\n"
 		   break
 		continue
-
+    #Catch some exceptions
     except IOError, e:
         if hasattr(e, 'code'): # HTTPError
             print "Error loading website " + r.url
@@ -149,5 +131,11 @@ def checkAtlas(wSite):
 
 #Hardcoded - need to specify in the function call which site you would like to crawl
 createSession("http://www.appfreak.net/list-of-100-mobile-game-review-sites/")
-print siteList
+print "Sites found: " + str(len(siteList)) + "\n"
+for foundSite in siteList:
+    print foundSite
+print "\n\n"
 checkHTTPS()
+print "The crawler successfully created rulesets for " + str(len(shortListedSites)) +" websites:\n"
+for shortSite in shortListedSites:
+	print shortSite
